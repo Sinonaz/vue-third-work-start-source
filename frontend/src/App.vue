67591b0676c55767ae6664e024c6e68ad23e6213
Isaac <aziksimpson@gmail.com>
2025-03-16 19:25:30 +0200

    2.10. Рефакторинг компонента HomeView.vue в проекте Vue-Work

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,24 @@
-<template>
-  <app-layout>
-    <home-view />
-  </app-layout>
-</template>
-
 <script setup>
 import { AppLayout } from "@/layouts";
 import { HomeView } from "@/views";
+import rawTasks from "@/mocks/tasks.json";
+import users from "@/mocks/users.json";
+import { normalizeTask } from "@/common/helpers";
+import { STATUSES } from "@/common/constants";
+
+const normalizedTasks = rawTasks.map((task) => normalizeTask(task));
+const filters = {
+  search: "",
+  users: users,
+  statuses: STATUSES,
+};
 </script>
+
+<template>
+  <app-layout>
+    <home-view :tasks="normalizedTasks" :filters="filters" />
+  </app-layout>
+</template>
 
 <style lang="scss">
 @import "@/assets/scss/app.scss";
